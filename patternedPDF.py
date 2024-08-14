04de53970cd73d8972b1d1254523b4e830660bde
Shubham <shubhamchauhan550@gmail.com>
2024-08-14 11:19:22 +0530

    changed name of pdf in python file

--- patternedPDF.py
+++ patternedPDF.py
@@ -8,6 +8,30 @@ class PatternedPDF:
     def __init__(self, output_folder, pdf_name, paper_width, paper_height, pattern, pattern2, grid_color, line_color,
                  background_color, table_color, grid_size, grid_line_width, line_width, cue_perc_left, cue_perc_right, summary_perc, title_perc,
                  rows, columns, margin):
+        """
+        Initialize the PatternedPDF object with given parameters.
+
+        :param output_folder: Directory to save the PDF
+        :param pdf_name: Name of the PDF file
+        :param paper_width: Width of the paper in mm
+        :param paper_height: Height of the paper in mm
+        :param pattern: Pattern type ('grid', 'dotted', 'ruled', 'blank')
+        :param pattern2: Additional pattern type ('table' or None)
+        :param grid_color: Color of the grid lines (list of RGB values)
+        :param line_color: Color of the lines (list of RGB values)
+        :param background_color: Background color of the page (list of RGB values)
+        :param table_color: Color of the table lines (list of RGB values)
+        :param grid_size: Size of the grid cells in mm
+        :param grid_line_width: Width of the grid lines in mm
+        :param line_width: Width of the lines in mm
+        :param cue_perc_left: Percentage position of the left cue line
+        :param cue_perc_right: Percentage position of the right cue line
+        :param summary_perc: Percentage position of the summary line
+        :param title_perc: Percentage position of the title line
+        :param rows: Number of rows for the table
+        :param columns: Number of columns for the table
+        :param margin: Page margins (list of 4 values: left, top, right, bottom)
+        """
         self.output_folder = output_folder
         self.pdf_name = pdf_name
         self.paper_width = paper_width * mm
@@ -31,14 +55,17 @@ class PatternedPDF:
         self.pdf_path = f'{output_folder}/{pdf_name}.pdf'
 
     def create_patterned_pdf(self):
+        # Create the PDF with the specified pattern and parameters
         os.makedirs(self.output_folder, exist_ok=True)
         pdf = canvas.Canvas(self.pdf_path, pagesize=[self.paper_width, self.paper_height])
         pdf.translate(0, self.paper_height)
         pdf.scale(1, -1)
         
+        # Set the background color
         pdf.setFillColorRGB(*self.background_color)
-        pdf.rect(0,0,self.paper_width,self.paper_height,fill=1, stroke = 0)
+        pdf.rect(0, 0, self.paper_width, self.paper_height, fill=1, stroke=0)
 
+        # Calculate the number of grid columns and rows
         self.grid_columns = int(self.paper_width / self.grid_size)
         self.grid_rows = int(self.paper_height / self.grid_size)
 
@@ -49,24 +76,28 @@ class PatternedPDF:
         #     if self.grid_rows % self.rows != 0:
         #         self.grid_rows = self.grid_rows // self.rows * self.rows
                 
+        # Adjust grid columns and rows based on margin and pattern
         self.pattern_width = self.paper_width - (self.margin[0] + self.margin[2])
         self.pattern_height = self.paper_height - (self.margin[1] + self.margin[3])
-
+        
         self.margin_horizontal = self.margin[0] + self.margin[2]
         self.margin_vertical = self.margin[1] + self.margin[3]
         self.grid_columns = int(self.pattern_width / self.grid_size)
         self.grid_rows = int(self.pattern_height / self.grid_size)
         
+        # Create the mesh grid for the pattern
         self.create_meshgrid()
         
         self.x_mesh_extended = self.x_mesh_extended + self.margin[0]
         self.y_mesh_extended = self.y_mesh_extended + self.margin[1]
         
+        # Set the grid color and line width
         pdf.setStrokeColor(Color(*self.grid_color))
         pdf.setFillColor(Color(*self.grid_color))
         pdf.setLineWidth(self.grid_line_width)
         r = self.line_width / 2
 
+        # Draw the specified pattern
         if self.pattern == 'grid':
             self.draw_grid(pdf)
         elif self.pattern == 'dotted':
@@ -76,15 +107,15 @@ class PatternedPDF:
         elif self.pattern == 'blank':
             self.draw_blank(pdf)
         
-        # Lines calculation
+        # Calculate line positions
         title_index = (self.title_perc * (self.grid_rows)) // 100
-        summary_index = self.grid_rows  - (self.summary_perc * (self.grid_rows)) // 100
+        summary_index = self.grid_rows - (self.summary_perc * (self.grid_rows)) // 100
         cue_index_left = int((self.cue_perc_left * (self.grid_columns)) // 100)
         cue_index_right = self.grid_columns - int((self.cue_perc_right * (self.grid_columns)) // 100)
-        self.cue_y1 = self.y_mesh_extended[0,0]
-        self.cue_y2 = self.y_mesh_extended[-1,0]
+        self.cue_y1 = self.y_mesh_extended[0, 0]
+        self.cue_y2 = self.y_mesh_extended[-1, 0]
 
-        # draw lines
+        # Draw lines
         pdf.setStrokeColor(Color(*self.line_color))
         pdf.setLineWidth(self.line_width)
         
@@ -93,17 +124,18 @@ class PatternedPDF:
         self.draw_cue_line(pdf, cue_index_left, self.cue_y1, self.cue_y2)
         self.draw_cue_line(pdf, cue_index_right, self.cue_y1, self.cue_y2)
         
+        # Draw table if pattern2 is 'table'
         pdf.setStrokeColor(Color(*self.table_color))
         
         if self.pattern2 == 'table':
             self.draw_table(pdf, title_index, cue_index_left, cue_index_right, summary_index)
         
         pdf.showPage()
-        # pdf.save()
         
         return pdf
         
     def create_meshgrid(self):
+        # Create a mesh grid based on the pattern width and height
         self.x_coordinates = np.arange(0, self.pattern_width, self.grid_size)
         self.y_coordinates = np.arange(0, self.pattern_height, self.grid_size)
 
@@ -114,6 +146,7 @@ class PatternedPDF:
         self.y_mesh_extended = np.concatenate([self.y_mesh_extended, self.y_mesh_extended[:, 0:1]], axis=1)
 
     def draw_horizontal_lines(self, pdf):
+        # Draw horizontal lines based on the mesh grid
         for row in self.y_mesh_extended[:, 0]:
             y = row
 
@@ -125,6 +158,8 @@ class PatternedPDF:
                 pdf.line(self.x1, y, self.x2, y)
 
     def draw_grid(self, pdf):
+        # Draw a grid pattern
+        
         # Horizontal lines
         self.x1 = self.x_mesh_extended[0, 0]
         self.x2 = self.x_mesh_extended[0, -1]
@@ -145,17 +180,18 @@ class PatternedPDF:
                 pdf.line(x, self.y1, x, self.y2)
 
     def draw_dotted(self, pdf):
+        # Draw a dotted pattern
         pdf.setLineCap(1)
         pdf.setDash([0.0000001, self.grid_size], 0)
         pdf.setLineWidth(self.line_width)
 
         # Horizontal dotted lines
         self.x1 = self.x_mesh_extended[0, 0]
-        self.x2 = self.x_mesh_extended[0, -1] 
+        self.x2 = self.x_mesh_extended[0, -1]
 
         if self.margin[1] == 0:
             self.x1 = self.x_mesh_extended[0, 1]
-        if self.margin[3] == 0 and self.x2  == self.paper_width:
+        if self.margin[3] == 0 and self.x2 == self.paper_width:
             self.x2 = self.x_mesh_extended[0, -1]
         if self.margin[1] == 0:
             self.x2 = self.x_mesh_extended[0, -1] + self.grid_size
@@ -166,16 +202,17 @@ class PatternedPDF:
         pdf.setDash([])
 
     def draw_ruled(self, pdf):
-        # Horizontal lines
+        # Draw ruled lines
         self.x1 = self.x_mesh_extended[0, 0]
         self.x2 = self.x_mesh_extended[0, -1]
         self.draw_horizontal_lines(pdf)
 
     def draw_blank(self, pdf):
+        # Draw a blank page (no pattern)
         pass
     
     def draw_table(self, pdf, title_index, cue_index_left, cue_index_right, summary_index):
-        
+        # Draw a table within the pattern
         if title_index != 0 and summary_index != 0:
             row_height = (self.y_mesh_extended[summary_index, 0] - self.y_mesh_extended[title_index, 0]) / self.rows
         elif title_index != 0:
@@ -209,7 +246,6 @@ class PatternedPDF:
         
         
         # draw horizontal lines
-        # x1 = self.x_mesh_extended[0,0]
         x2 = self.x_mesh_extended[0,-1]
         
         if cue_index_left == 0:
@@ -273,7 +309,7 @@ class PatternedPDF:
 
 
 # Example Usage:
-output_folder = 'A4_template_5 mm'
+output_folder = 'templates/A4 5mm white'
 pdf_name = 'template 5 mm'      # if only single pdf is to be created
 
 paper_width = 210
@@ -304,15 +340,20 @@ rows = 1
 columns = 2
 margin = np.array([0, 0, 0, 0])
 
-# title_perc = np.array([0])
-# cue_perc_left = np.array([0])
-# summary_perc = np.array([0])
+''' For single pdf '''
+
+# title_perc = np.array([5])
+# cue_perc_left = np.array([5])
+# cue_perc_right = np.array([10])
+# summary_perc = np.array([10])
 # patterned_pdf = PatternedPDF(output_folder, pdf_name, paper_width, paper_height, pattern, pattern2, grid_color, line_color,
-#                                 background_color, table_color, grid_size, grid_line_width, line_width, cue_perc_left, summary_perc, title_perc,
+#                                 background_color, table_color, grid_size, grid_line_width, line_width, cue_perc_left, cue_perc_right, summary_perc, title_perc,
 #                                 rows, columns, margin)
 # doc = patterned_pdf.create_patterned_pdf()
 # doc.save()
 
+
+''' For multiple pdf '''
 
 patterns = ['grid', 'dotted', 'blank', 'ruled']
 
@@ -321,10 +362,7 @@ for title in title_perc:
         for cue_right in cue_perc_right:
             for summary in summary_perc:
                 for pattern in patterns:
-                    if pattern2 == 'table':
-                        pdf_name = f'title_{title}_cue_{cue_left}_{cue_right}_summary_{summary}_column_{rows}x{columns}_{pattern}'
-                    else:
-                        pdf_name = f'title_{title}_cue_{cue_left}_{cue_right}_summary_{summary}_{pattern}'
+                    pdf_name = f'{pattern} - (title, cue (L-R), summary) - ({title}, {cue_left}-{cue_right}, {summary})%'
 
                     patterned_pdf = PatternedPDF(output_folder, pdf_name, paper_width, paper_height, pattern, pattern2, grid_color, line_color,
                                                 background_color, table_color, grid_size, grid_line_width, line_width, cue_left, cue_right, summary, title,
@@ -345,10 +383,7 @@ for title in title_perc:
             for summary in summary_perc:
                 for pattern in patterns:
                     if cue_left != cue_right:
-                        if pattern2 == 'table':
-                            pdf_name = f'title_{title}_cue_{cue_left}_{cue_right}_summary_{summary}_column_{rows}x{columns}_{pattern}'
-                        else:
-                            pdf_name = f'title_{title}_cue_{cue_left}_{cue_right}_summary_{summary}_{pattern}'
+                        pdf_name = f'{pattern} - (title, cue (L-R), summary) - ({title}, {cue_left}-{cue_right}, {summary})%'
                         
                         patterned_pdf = PatternedPDF(output_folder2, pdf_name, paper_width, paper_height, pattern, pattern2, grid_color, line_color,
                                                     background_color, table_color, grid_size, grid_line_width, line_width, cue_left, cue_right, summary, title,
@@ -371,7 +406,7 @@ for title in title_perc:
         for cue_right in cue_perc_right:
             for summary in summary_perc:
                 for pattern in patterns:
-                    pdf_name = f'{pattern}_column_{rows}x{columns}'
+                    pdf_name = f'{pattern} - table {rows}x{columns}'
                     
                     patterned_pdf = PatternedPDF(output_folder2, pdf_name, paper_width, paper_height, pattern, pattern2, grid_color, line_color,
                                                 background_color, table_color, grid_size, grid_line_width, line_width, cue_left, cue_right, summary, title,
